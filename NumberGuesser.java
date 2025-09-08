1362981621c5ca1c807decff668d2b00f8cbef12
KeaganMcclelland <130119568+KeaganMcclelland@users.noreply.github.com>
2025-09-07 20:23:54 -0500

    Update NumberGuesser.java

--- NumberGuesser.java
+++ NumberGuesser.java
@@ -1,33 +1,38 @@
-import java.util.Random;
 import java.util.Scanner;
-
+import java.util.Random;
 public class tut {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
-
-        int randomNumber = rand.nextInt(100) + 1;
-        int userGuess = 0;
-        int attempts = 0;
-        boolean guessedCorrectly = false;
-
-        while (!guessedCorrectly) {
-            System.out.print("Enter a number: ");
-            userGuess = sc.nextInt(); 
-            attempts++;
-
-            if (userGuess == randomNumber) {
-                System.out.println("Congratulations! You guessed correctly.");
-                System.out.println("It took you " + attempts + " attempts.");
-                guessedCorrectly = true; 
-            } else if (userGuess < randomNumber) {
-                System.out.println("Too low! Try again.");
-            } else {
-                System.out.println("Too high! Try again.");
-            }
-        }
-
-        sc.close();
-    } 
-}
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+				Random rand = new Random();
+					int randomNumber = rand.nextInt(100) + 1;
+					int userGuess = 0; int attempts = 0;
+					boolean guessedCorrectly = false;
+					
+			while (!guessedCorrectly) { System.out.print("Enter a number: ");
+					userGuess = sc.nextInt();
+						attempts++;
+						
+						
+				if (userGuess == randomNumber) { guessedCorrectly = true;
+					System.out.println("Congratulations! You guessed correctly.");
+		System.out.println("It took you " + attempts + " attempts.");
+					int playerscore = 0;
+						playerscore = calculateScore(attempts);
+		System.out.println("Your Score:" + playerscore);
+		
+			} else if (userGuess < randomNumber)
+	{ System.out.println("Too low! Try again.");
+			} else { System.out.println("Too high! Try again.");
+				}
+		
+		
+		} sc.close();
+	
+	
+	} public static int calculateScore(int attempts) {
+			int maxScore = 100;
+			int score = maxScore - attempts;
+				if (score < 0)
+				{ score = 0; } return score;
+			}
+		}
