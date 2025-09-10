b896fa41ec1b8b2eb00fd7c4df7c30cf4ee25984
KeaganMcclelland <130119568+KeaganMcclelland@users.noreply.github.com>
2025-09-09 21:01:58 -0500

    Array.java

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        
+        long[] numbers = new long[3];
+        numbers[0] = 90;
+        numbers[1] = 82;
+        numbers[2] = 83;
+        long numbersF = numbers[0] + numbers[1] + numbers[2];
+        long numberD = numbersF / 3;
+        System.out.println("Index: " + numberD);
+    
+    }
+} //Simple Array Grade Storage
